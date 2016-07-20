1721cb8347291b377abac6ba8892f5feb7806273
twitchyliquid64 <twitchyliquid64@ciphersink.net>
2016-07-20 07:13:28 +0000

    Moar git

--- config/shell.sh
+++ config/shell.sh
@@ -5,16 +5,17 @@
 shopt -s promptvars dotglob histappend no_empty_cmd_completion cdspell xpg_echo
 
 
+function status__
+{
+  echo `git status` | grep "nothing to commit" > /dev/null 2>&1
+  if [ "$?" -eq "1" ]; then
+    printf "[dirty]"
+  fi
+}
 
-function parse_git_dirty {
-  echo -n $(git status 2>/dev/null | awk -v out=$1 -v std="[dirty]" '{ if ($0=="# Changes to be committed:") std="uncommited"; last="a" } END{ if(last!="" && last!="nothing to commit (working directory clean)") { if(out!="") print out; else print std } }')
-}
-function parse_git_remote {
-  echo -n $(git status 2>/dev/null | awk -v out=$1 '/# Your branch is / { if(out=="") print "[ahead]"; else print out }')
-}
 alias branchname="git branch 2>/dev/null | sed -ne 's/^* \(.*\)/ ${PARENCLR}(${BRANCHCLR}\1${PARENCLR}\)/p'"
 
-export PS1='$GREEN[\u@\h]$NC:$LIGHTBLUE\w$NC$CYAN$(branchname)$YELLOW$(parse_git_dirty)$(parse_git_remote)$WHITE>$NC'
+export PS1='$GREEN[\u@\h]$NC:$LIGHTBLUE\w$NC$CYAN$(branchname)$YELLOW$(status__)$WHITE>$NC'
 
 
 
