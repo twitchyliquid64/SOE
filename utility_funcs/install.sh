0b5b4db784c4c667d61c060a8c5cc5aada8e6592
Tom <twitchyliquid64@ciphersink.net>
2019-09-11 16:11:02 -0700

    Improve install-go

--- utility_funcs/install.sh
+++ utility_funcs/install.sh
@@ -1,7 +1,9 @@
 #@function install-go
-#@description Installs go 1.12.
+#@description Installs go 1.12.9.
 install-go ()  {
   ARCH="$(uname -m)"
+  VERS="1.12.9"
+  BASE_PATH="/usr/local/go"
 
   case $ARCH in
   "x86_64")
@@ -15,14 +17,29 @@ install-go ()  {
       ;;
   esac
 
-  URL="https://storage.googleapis.com/golang/go1.12.9.linux-${ARCH}.tar.gz"
+  URL="https://storage.googleapis.com/golang/go${VERS}.linux-${ARCH}.tar.gz"
   wget $URL -O /tmp/go.tar.gz
   if [ $? -ne 0 ]; then
     echo "Failed to download ${URL}"
-    exit 1
-  else
-    echo "Extracting ${URL##*/}"
-    mkdir -pv "/usr/local/go"
-    tar -C "/usr/local/go" --strip-components=1 -xzf /tmp/go.tar.gz
+    return
+  fi
+
+  if [[ -d $BASE_PATH ]]; then
+    echo "Deleting ${BASE_PATH}, which already exists."
+    sudo rm -rfv $BASE_PATH
+  fi
+
+  echo "Extracting ${URL##*/}"
+  sudo mkdir -pv $BASE_PATH
+  sudo tar -C $BASE_PATH --strip-components=1 -xzf /tmp/go.tar.gz
+
+  if [[ $PATH != *"${BASE_PATH}/bin"* ]]; then
+    echo "Adding ${BASE_PATH}/bin to regular path"
+    export PATH="${PATH}:${BASE_PATH}/bin"
+  fi
+
+  if [[ ( ! -f ~/.bash_profile ) || $(cat ~/.bash_profile) != *"/usr/local/go/bin"* ]]; then
+    echo "Adding /usr/local/go/bin to ~/.bash_profile"
+    echo 'export PATH=/usr/local/go/bin:$PATH' >>~/.bash_profile
   fi
 }
